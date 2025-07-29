bab5e1b0bcf7966514d4af57d03df0d6239bb5a0
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-07-29 22:55:53 +0530

    initial commit

---
+++ pattern.py
@@ -0,0 +1,8 @@
+# solving 22 patterns problems
+
+# tips
+# 1. for the outer loop, count the no. of lines
+# 2. for the inner loop, focus on the columns and connect with them somehow to the rows
+# 3. print them " * " inside the inner for loop
+# 4. observe symmetry { optional }
+
