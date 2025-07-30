95efcc32a74ca5c5aa1194a3c5ba44f0369f0271
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-07-31 00:27:49 +0530

    pattern6

--- pattern.py
+++ pattern.py
@@ -64,14 +64,14 @@ def pattern5(n):
 # *
 def pattern6(n):
     for i in range (n,0,-1):
-        for j in range(i,0,-1):
+        for j in range(1,i+1):
             print(j,end="")
         print()
 # enter n:5
-# 54321
-# 4321
-# 321
-# 21
+# 12345
+# 1234
+# 123
+# 12
 # 1
 n=int(input("enter n:"))
 pattern6(n)
