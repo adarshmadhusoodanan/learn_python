75154a987e513682b5d247daeb3e2cac47629cff
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-08-04 23:12:21 +0530

    pattern15

--- pattern.py
+++ pattern.py
@@ -202,9 +202,16 @@ def pattern14(n):
 # 4 5 6
 # 7 8 9 10
 # 11 12 13 14 15
+def pattern15(n):
+    for i in range(n):
+        for j in range(i+1):
+            print(chr(65+j),end=" ")
+        print()
+# enter n:5
+# A 
+# A B
+# A B C
+# A B C D
+# A B C D E
 n=int(input("enter n:"))
-pattern14(n)
-
-
-
-
+pattern15(n)
