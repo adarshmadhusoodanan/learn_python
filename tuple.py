913fb738e08b3fa4db9f890d6e0536140f4bdf4d
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-08-17 09:06:23 +0530

    tuple1

--- tuple.py
+++ tuple.py
@@ -4,8 +4,12 @@
 # t = (,)               SyntaxError: invalid syntax
 # t = (10)              10 <class 'int'>
 
-# t = 10,               (10,) <class 'tuple'>               define without paranthesis
-# t = (10,)             (10,) <class 'tuple'>               define
+# t = 10,               (10,) <class 'tuple'>                define without paranthesis
+# t = (10,)             (10,) <class 'tuple'>                define
+# t=()                  () <class 'tuple'>                   empty tuple
+# t=(1,3,5,(2,4,6))     (1, 3, 5, (2, 4, 6)) <class 'tuple'> nested tuple
+
+
 
             # property of tuples
 
@@ -21,7 +25,10 @@
             # tuple supports indexing
 
 # t = 1,2,3,4
-# print(t[1])   op: 2
+# print(t[1])     op: 2
+# t=(1,3,5,(2,4,6)) 
+# print(t[3])     op: (2, 4, 6)
+# print(t[3][1])  op: 4
 
             # tuple supports slicing
 
@@ -31,4 +38,10 @@
 # print(t[1::2])    op: (2, 4)
 # print(t[:3])      op: (1, 2, 3)
 
+            # tuple built-in functions
+# t=(1,2,3,1,4,1,5)
+# print(t.count(1))     op:3 
 
+# print(t.index(1))     op:0
+# print(t.index(1,4))   op:5
+# print(t.index(1,1,4)) op:3
