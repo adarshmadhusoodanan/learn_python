dded76a1ffdc070bf3dd3e804ed1a2a3746f0b84
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-08-18 14:49:58 +0530

    list can contain int,float,complex,str,list,tuple,set,dict

--- list.py
+++ list.py
@@ -1,51 +1,63 @@
 # defining list
 
-# l1=[1,2,3,4,5]       [1, 2, 3, 4, 5] <class 'list'>       define
-# l1=[]                [] <class 'list'>                    empty list
+# l1=[1,2,3,4,5]                                            [1, 2, 3, 4, 5] <class 'list'>       define
+# l1=[]                                                     [] <class 'list'>                    empty list
 
-# l1=[0,2,4,[1,3,5]]   [0, 2, 4, [1, 3, 5]] <class 'list'>  nested list
-# print(len(l1))       op: 4                                length of list
+# l1=[0,2,4,[1,3,5]]                                        [0, 2, 4, [1, 3, 5]] <class 'list'>  nested list
+# print(len(l1))                                            op: 4                                length of list
+
+# l1=[1, 2, 3, (1, 2, 3)]                                   
+# print(type(l1[3]))                                        <class 'tuple'>
+# print(l1,type(l1))                                        [1, 2, 3, (1, 2, 3)] <class 'list'>
+
+# l1=[1, 2, 3, {1, 2, 3}]                                  
+# print(type(l1[3]))                                        <class 'set'>
+# print(l1,type(l1))                                        [1, 2, 3, {1, 2, 3}] <class 'list'>
+
+# l1=[1,2.3,2+4j,'a'== 'a', {'a':"apple" , 'b':"bat"} ]
+# print(type(l1[4]))                                        <class 'dict'>
+# print(l1,type(l1))                                        [1, 2.3, (2+4j), True, {'a': 'apple', 'b': 'bat'}] <class 'list'>
 
 # property of list
             # list can be homogenous or heterogenous 
-# l1=[1,2,3,4,5]         [1, 2, 3, 4, 5] <class 'list'>             homogenous collection
-# l1=[1,3.4,"don",2+3j]  [1, 3.4, 'don', (2+3j)] <class 'list'>     heterogenous collection
+# l1=[1,2,3,4,5]                                            [1, 2, 3, 4, 5] <class 'list'>             homogenous collection
+# l1=[1,3.4,"don",2+3j]                                     [1, 3.4, 'don', (2+3j)] <class 'list'>     heterogenous collection
 
             # list can have duplicate elements
 
-# l1=[0,1,1,2,3,4]       [0, 1, 1, 2, 3, 4] <class 'list'>          have duplicate elements
+# l1=[0,1,1,2,3,4]                                          [0, 1, 1, 2, 3, 4] <class 'list'>          have duplicate elements
 
             # list are ordered collection
 
-# l1=[0,2,4,[1,3,5]]     [0, 2, 4, [1, 3, 5]] <class 'list'>        
+# l1=[0,2,4,[1,3,5]]                                        [0, 2, 4, [1, 3, 5]] <class 'list'>        
 
             # list support indexing
 
 # l1=[0,2,4,[1,3,5]]
-# print(l1[0])           op: 0
-# print(l1[3])           op: [1, 3, 5]
-# print(l1[3][1])        op: 3
-# print(l1[-1][-1])      op: 5
+# print(l1[0])                      op: 0
+# print(l1[3])                      op: [1, 3, 5]
+# print(l1[3][1])                   op: 3
+# print(l1[-1][-1])                 op: 5
 
             # list support slicing
 
 # l1=[0,2,4,[1,3,5]]
-# print(l1[1:3])         op: [2, 4]
-# print(l1[2:4])         op: [4, [1, 3, 5]]
-# print(l1[0:4:2])       op: [0, 4]
-# print(l1[:3])          op: [0, 2, 4]
-# print(l1[::-1])        op: [[1, 3, 5], 4, 2, 0]
-# print(l1[::])          op: [0, 2, 4, [1, 3, 5]]
-# print(l1[4:0:-1])      op: [[1, 3, 5], 4, 2]
-# print(l1[-1:-3:-1])    op: [[1, 3, 5], 4]
-# print(l1[-4:-1:1])     op: [0, 2, 4]
-# print(l1[3][0:2])      op: [1, 3]
+# print(l1[1:3])                    op: [2, 4]
+# print(l1[2:4])                    op: [4, [1, 3, 5]]
+# print(l1[0:4:2])                  op: [0, 4]
+# print(l1[:3])                     op: [0, 2, 4]
+# print(l1[::-1])                   op: [[1, 3, 5], 4, 2, 0]
+# print(l1[::])                     op: [0, 2, 4, [1, 3, 5]]
+# print(l1[4:0:-1])                 op: [[1, 3, 5], 4, 2]
+# print(l1[-1:-3:-1])               op: [[1, 3, 5], 4]
+# print(l1[-4:-1:1])                op: [0, 2, 4]
+# print(l1[3][0:2])                 op: [1, 3]
 
             # list are mutable
 
 # l1=[0,2,4,[1,3,5]]
-# l1[1]="iron man"       op: [0, 'iron man', 4, [1, 3, 5]]
-# l1[3][2]="6.7"         op: [0, 2, 4, [1, 3, '6.7']]
+# l1[1]="iron man"                  op: [0, 'iron man', 4, [1, 3, 5]]
+# l1[3][2]="6.7"                    op: [0, 2, 4, [1, 3, '6.7']]
 
 # list 11 built-in functions
 
@@ -53,140 +65,140 @@
 
 # l1=[1,2,3,4,5]
 # l1.clear()
-# print(l1)              op: []
-# print(l1.clear())      op: None
+# print(l1)                         op: []
+# print(l1.clear())                 op: None
 
             # list.copy() -> list
 
 # l1=[1,2,3,4,5]
 # l2=l1.copy()
-# print(l2)              op: [1, 2, 3, 4, 5]
-# l2[3]=5                creates shallow copy 
-# print(l2)              op: [1, 2, 3, 5, 5] 
-# print(l1)              op: [1, 2, 3, 4, 5]
+# print(l2)                         op: [1, 2, 3, 4, 5]
+# l2[3]=5                           creates shallow copy 
+# print(l2)                         op: [1, 2, 3, 5, 5] 
+# print(l1)                         op: [1, 2, 3, 4, 5]
 
             # list.count(value) -> int
 
 # l1=[1,2,3,4,5,1]
-# print(l1.count(1))     op: 2
+# print(l1.count(1))                op: 2
 
             # list.sort() -> None
 
 # l1=[1,4,7,8,4,3,2]
 # l1.sort()
-# print(l1)                  op: [1, 2, 3, 4, 4, 7, 8]
-# print(l1.sort())           op: None
+# print(l1)                         op: [1, 2, 3, 4, 4, 7, 8]
+# print(l1.sort())                  op: None
 
 # l1=["list","tuple","set","dic"]
 # l1.sort()
-# print(l1)                  op: ['dic', 'list', 'set', 'tuple']
+# print(l1)                         op: ['dic', 'list', 'set', 'tuple']
 
 # l1.sort(reverse=True)
-# print(l1)                  op: [8, 7, 4, 4, 3, 2, 1]
+# print(l1)                         op: [8, 7, 4, 4, 3, 2, 1]
 
 # l1=[1,4,6,7,3.4]
 # l1.sort()
-# print(l1)                  op: [1, 3.4, 4, 6, 7]
+# print(l1)                         op: [1, 3.4, 4, 6, 7]
 
 # l1=[10,2,9,4,[0,9,7,3]]
 # l1.sort()
-# print(l1)                  op: TypeError: '<' not supported between instances of 'list' and 'int'
+# print(l1)                         op: TypeError: '<' not supported between instances of 'list' and 'int'
 
 # l1=[1,4,6,7,3.4,"sort","dice"]
 # l1.sort()
-# print(l1)                  op: TypeError: '<' not supported between instances of 'str' and 'int'
+# print(l1)                         op: TypeError: '<' not supported between instances of 'str' and 'int'
 
 # l1 =[1,2,3+4j]
 # l1.sort()
-# print(l1)                  op: TypeError: '<' not supported between instances of 'complex' and 'int'
+# print(l1)                         op: TypeError: '<' not supported between instances of 'complex' and 'int'
 
             # list.reverse() -> None
 
 # l1=[1,4,7,8,4,3,2]
 # l1.reverse()
-# print(l1)                  op: [2, 3, 4, 8, 7, 4, 1]
-# print(l1.reverse())        op: None
+# print(l1)                         op: [2, 3, 4, 8, 7, 4, 1]
+# print(l1.reverse())               op: None
 
 # l1=["list","tuple","set","dic"]
 # l1.reverse()
-# print(l1)                  op: ['dic', 'set', 'tuple', 'list']
+# print(l1)                         op: ['dic', 'set', 'tuple', 'list']
 
 # l1=[1,3,3.4,3+4j,"list"]
 # l1.reverse()
-# print(l1)                  op: ['list', (3+4j), 3.4, 3, 1]
+# print(l1)                         op: ['list', (3+4j), 3.4, 3, 1]
 
             # l1.index(value,start:0,stop:size of list)-> int
 
 # l1=[3,2,3,[4,"don",8.5,3],3]
-# print(l1.index(3))            op: 0
+# print(l1.index(3))                op: 0
 
-# print(l1.index(8.5))          op: ValueError: 8.5 is not in list
+# print(l1.index(8.5))              op: ValueError: 8.5 is not in list
 
-# print(l1[3].index(8.5))       op: 2
+# print(l1[3].index(8.5))           op: 2
 
-# print(l1.index(3,1,5))        op: 2
+# print(l1.index(3,1,5))            op: 2
 
             # list.append(value/object) -> None
 
 # l1=[1,2,3,4,5]
 # l1.append(6)
-# print(l1)                op: [1, 2, 3, 4, 5, 6]
+# print(l1)                         op: [1, 2, 3, 4, 5, 6]
 
-# print(l1.append(6))      op: None
+# print(l1.append(6))               op: None
 
 # l1.append([5,6,7])       
-# print(l1)                op: [1, 2, 3, 4, 5, [5, 6, 7]]
+# print(l1)                         op: [1, 2, 3, 4, 5, [5, 6, 7]]
 
 # l1.append("don")
-# print(l1)                op: [1, 2, 3, 4, 5, 'don']
+# print(l1)                         op: [1, 2, 3, 4, 5, 'don']
 
 # l1.append(1,2)
-# print(l1)                op: TypeError: list.append() takes exactly one argument (2 given)
+# print(l1)                         op: TypeError: list.append() takes exactly one argument (2 given)
 
             # list.extend(iterable) -> None
 
 # l1=[1,2,3,4]
 # l1.extend([5,6,7])
-# print(l1)                     op:[1, 2, 3, 4, 5, 6, 7]
+# print(l1)                         op:[1, 2, 3, 4, 5, 6, 7]
 
-# print(l1.extend([1,3,5]))     op: None
+# print(l1.extend([1,3,5]))         op: None
 
 # l1.extend("list")
-# print(l1)                     op: [1, 2, 3, 4, 'l', 'i', 's', 't']
+# print(l1)                         op: [1, 2, 3, 4, 'l', 'i', 's', 't']
 
 # l1.extend([5])
-# print(l1)                     op: [1, 2, 3, 4, 5]
+# print(l1)                         op: [1, 2, 3, 4, 5]
 
 # l1.extend(1)
-# print(l1)                     op: TypeError: 'int' object is not iterable
+# print(l1)                         op: TypeError: 'int' object is not iterable
 
             # list.insert(index,value) -> None
 
 # l1=[1,2,3,4,5]
 # l1.insert(3,5)                  
-# print(l1)                     op: [1, 2, 3, 5, 4, 5]
+# print(l1)                         op: [1, 2, 3, 5, 4, 5]
 
-# print(l1.insert(3,5))         op: None
+# print(l1.insert(3,5))             op: None
 
             # list.pop(index:-1)-> Any
 
 # l1=[1,2,3,4,5]
-# print(l1.pop(1))              op: 2
-# print(l1)                     op: [1, 3, 4, 5]
+# print(l1.pop(1))                  op: 2
+# print(l1)                         op: [1, 3, 4, 5]
 
-# print(l1.pop())               op: 5
-# print(l1)                     op: [1, 2, 3, 4]
+# print(l1.pop())                   op: 5
+# print(l1)                         op: [1, 2, 3, 4]
             
-# print(l1.pop(-2))             op: 4
-# print(l1)                     op: [1, 2, 3, 5]
+# print(l1.pop(-2))                 op: 4
+# print(l1)                         op: [1, 2, 3, 5]
 
-# print(l1.pop(5))              op: IndexError: pop index out of range
+# print(l1.pop(5))                  op: IndexError: pop index out of range
 
             # list.remove(value) -> None
 
 # l1=[1,5,2,3,4,5]
 # l1.remove(5)
-# print(l1)                     op: [1, 2, 3, 4, 5]
+# print(l1)                         op: [1, 2, 3, 4, 5]
 
-# print(l1.remove(5))           op: None
-# l1.remove()                   op: list.remove() takes exactly one argument
+# print(l1.remove(5))               op: None
+# l1.remove()                       op: list.remove() takes exactly one argument
