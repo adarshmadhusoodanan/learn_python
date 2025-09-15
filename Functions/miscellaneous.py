1aa3f3e8904c5d489772c2a83ff19fb6f25fddcb
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-09-16 02:18:18 +0530

    miscellaneous example

--- Functions/miscellaneous.py
+++ Functions/miscellaneous.py
@@ -13,3 +13,15 @@ greeting()
         # Hello
         # Hello
 
+def outer_function():   
+    print("i am outer function")
+    def inner_function():
+        print("i am inner function")
+    return inner_function
+inner_func = outer_function()   # here inner_func is a variable which holds the reference of inner_function
+print(inner_func)
+inner_func()                    # calling inner_function using inner_func variable
+# output
+        # i am outer function   
+        # <function outer_function.<locals>.inner_function at 0x0000026B7719D260>
+        # i am inner function
