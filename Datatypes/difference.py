b357ee7de131a671915eb32b78291a2b3bb7eb20
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-09-19 23:08:12 +0530

    difference

--- Datatypes/difference.py
+++ Datatypes/difference.py
@@ -27,23 +27,5 @@
 # set update() method is similar to list extend() method but not same as list append() method because append() method adds single element at the end of the list where as update() method adds multiple elements at once from iterable data types like list, tuple, set, dict, str
 # dict update() method is similar to list extend() method but not same as list append() method because append() method adds single element at the end of the list where as update() method adds key-value pairs from another dictionary
 
-# some methods are common in list, set and dict like clear(), copy(), pop(), remove() but their functionality is different in each data type
-# some methods are common in list and set like add(), difference(), discard(), intersection(), isdisjoint(), issubset(), issuperset(), symmetric_difference(), union() but their functionality is different in each data type
-# some methods are common in list and dict like get(), items(), keys(), values() but their functionality is different in each data type
-# some methods are common in set and dict like fromkeys() but their functionality is different in each data type
-
-
-#built-in methods main differences in each data type
-# list methods: append(), extend(), insert(), sort(), reverse(), index(), count()
-# tuple methods: count(), index()
-# set methods: add(), difference(), discard(), intersection(), isdisjoint(), issubset(), issuperset(), symmetric_difference(), union()
-# dict methods: fromkeys(), get(), items(), keys(), values()
-
-
-# in common methods like update() but there is difference in functionality in each data type
-# list update() method is not there 
-# set update() method is similar to list extend() method but not same as list append() method because append() method adds single element at the end of the list where as update() method adds multiple elements at once from iterable data types like list, tuple, set, dict, str
-# dict update() method is similar to list extend() method but not same as list append() method because append() method adds single element at the end of the list where as update() method adds key-value pairs from another dictionary
-# tuple update() method is not there
 #------------------------------------------------------------------------------------------------------------------------------------------------
 
